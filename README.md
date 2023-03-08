88a0d90e44cabf82c2cfefb7625af79b78200557
chamaedorea-productions <chamaedorea.productions@outlook.de>
2023-03-08 12:56:39 +0100

    Created basic layout

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # flow-free-clone
- 
+
+# Compile TypeScript 
+ ```sh
+  tsc -p ./tsconfig.json
+ ``` 
+
+# Compile TailwindCss
+ ```sh
+  tailwindcss -i ./css/main.css -o ./css/dist/main.css
+ ```
---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="./", static_folder="/")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+app.run(port=5000, host="127.0.0.1", debug=True)
